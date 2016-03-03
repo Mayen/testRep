921035629a8fab86cbecddf24936173d5a0fba8f
mmayen <mainuddin.mahmmed@gmail.com>
2016-03-03 19:57:42 +0600

    deleted

--- saml-project-new/src/com/bcbs/saml/grcode/SAMLResponseWriter.java
+++
@@ -1,8 +0,0 @@
-package com.bcbs.saml.grcode;
-
-public interface SAMLResponseWriter {
-	
-	public String getSAMLResponse(SAMLData samlBaseData);
-	public String getSAMLAssertion(SAMLData samlData);
-
-}
